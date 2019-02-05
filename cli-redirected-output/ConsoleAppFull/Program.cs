c9fe90008586ef2d2e07805491e2af29939255ee
Eugene Strizhok <eugene.strizhok@jetbrains.com>
2019-02-05 14:27:15 +0100

    cli-redirected-output repro

---
+++ cli-redirected-output/ConsoleAppFull/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleAppFull
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var startInfo = new ProcessStartInfo
+      {
+        FileName = @"dotnet.exe",
+        Arguments = @"exec ""..\..\..\ConsoleAppNetCore\bin\Debug\netcoreapp2.1\ConsoleAppNetCore.dll""",
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        RedirectStandardInput = true
+      };
+
+      var process = Process.Start(startInfo);
+      var output = process.StandardOutput.ReadToEnd();
+
+      process.WaitForExit();
+
+      Console.WriteLine("Output read: " + output);
+
+      if (File.Exists("error.txt"))
+        Console.WriteLine("Error in started process: " + File.ReadAllText("error.txt"));
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ cli-redirected-output/ConsoleAppNetCore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleAppNetCore
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      File.Delete("error.txt");
+
+      try
+      {
+        Console.WriteLine($"Width: {Console.WindowWidth}");
+        Console.WriteLine("Hello World!");
+      }
+      catch (Exception e)
+      {
+        File.WriteAllText("error.txt", e.Message + Environment.NewLine + e.StackTrace);
+      }
+    }
+  }
+}
